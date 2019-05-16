206a4d9f0d00d1c71a825ed348b24bff24796535
raylison nunes dos santos <raylison100@gmail.com>
2019-05-16 07:14:02 -0300

    Ajuste filtro Cross

---
+++ src/main/java/br/com/betterworld/filter/SimpleCORSFilter.java
@@ -0,0 +1,46 @@
+package br.com.betterworld.filter;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCORSFilter implements Filter {
+
+    private final Log logger = LogFactory.getLog(this.getClass());
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+        logger.info("USerSpringSecurity | SimpleCORSFilter loaded");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
