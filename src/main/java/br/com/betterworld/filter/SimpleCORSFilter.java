4a85da66ee8ec8f9b6b2caa76e7b68251f3aecc5
raylison nunes dos santos <raylison100@gmail.com>
2019-05-16 11:03:23 -0300

    Ajuste filtro Cross

--- src/main/java/br/com/betterworld/filter/SimpleCORSFilter.java
+++ src/main/java/br/com/betterworld/filter/SimpleCORSFilter.java
@@ -31,11 +31,11 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
 
-//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            chain.doFilter(req, resp);
-//        }
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
 
     }
 
